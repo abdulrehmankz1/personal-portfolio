8baccda16dabdb3e4fed03143f34218e67e7308d
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-02-22 22:49:40 +0500

    add intro section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import IntroSection from './components/IntroSection';
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className='container-fluid py-5 my-3'>
       <Sidebar />
+      <IntroSection />
     </div>
   );
 }
---
+++ src/components/IntroSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function IntroSection() {
+  return (
+    <div className='custom-container'>
+      <span class="badge rounded-pill section_badge">
+        <i class="fa-solid fa-house"></i>
+        &nbsp; INTRODUCE
+      </span>
+      <h1 className='text-light'>
+        Say Hi from <span className='hero_content'>
+          Abdul Rehman
+        </span>, FrontEnd Developer
+      </h1>
+    </div>
+  )
+}
+
+export default IntroSection
