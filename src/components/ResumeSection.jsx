6a382f15f0e658e493bf819b273ad1d76a1f2d94
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-02-27 16:29:12 +0500

    add resume section

--- src/components/ResumeSection.jsx
+++ src/components/ResumeSection.jsx
@@ -30,6 +30,34 @@ function ResumeSection() {
           </div>
 
         </div>
+        <div className='timeline_card'>
+          <div className='dots'></div>
+          <div className='ms-5 ps-3'>
+            <span className='date '>
+              2020 - Present
+            </span>
+            <h2 className='title mb-0'>
+              Front-End Developer
+            </h2>
+            <p className='sub_title '>
+              Envato Market
+            </p>
+          </div>
+        </div>  <div className='timeline_card'>
+          <div className='dots'></div>
+          <div className='ms-5 ps-3'>
+            <span className='date '>
+              2020 - Present
+            </span>
+            <h2 className='title mb-0'>
+              Front-End Developer
+            </h2>
+            <p className='sub_title '>
+              Envato Market
+            </p>
+          </div>
+
+        </div>
       </div>
     </div>
   )
