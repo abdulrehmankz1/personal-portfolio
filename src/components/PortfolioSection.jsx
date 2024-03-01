cd3c3de7d76a4c1cc07692684e4544dd84f6ede5
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-03-01 19:42:05 +0500

    add carousel

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 
 function PortfolioSection() {
-  const [showCarousel, setShowCarousel] = useState(false);
+  const [showEduportCarousel, setShowEduportCarousel] = useState(false);
+  const [showNetflixCarousel, setShowNetflixCarousel] = useState(false);
 
-  const handlePortfolioItemClick = () => {
-    setShowCarousel(true);
+  const handleEduportItemClick = () => {
+    setShowEduportCarousel(true);
+  };
+
+  const handleNetflixItemClick = () => {
+    setShowNetflixCarousel(true);
   };
 
   const handleCloseCarousel = () => {
-    setShowCarousel(false);
+    setShowEduportCarousel(false);
+    setShowNetflixCarousel(false);
   };
 
   return (
@@ -23,9 +29,10 @@ function PortfolioSection() {
         </h2>
       </div>
       <div className="row">
-        <div className="portfolio_item col-11" onClick={handlePortfolioItemClick}>
+        <div className="portfolio_item col-11" onClick={handleEduportItemClick}>
           <div className="portfolio_item_inner">
-            <div className="eduport">
+            <div id='eduport' className="eduport">
+              <img src="./assets/images/eduport-1.png" className='w-100 rounded-4' alt="" />
               <ul className="portfolio_categories">
                 <li>
                   <a href="">Figma</a>
@@ -45,13 +52,15 @@ function PortfolioSection() {
           </div>
           <h2><a href="https://eduport-black.vercel.app" target="_blank" rel="noopener noreferrer" className='text-light'>Bureau - Architecture Studio Website</a></h2>
         </div>
-
-        {showCarousel && (
+        {showEduportCarousel && (
           <div className="carousel">
             <span className="close-button" onClick={handleCloseCarousel}>&times;</span>
-            <div id="carouselExampleFade" className="carousel slide carousel-fade w-75 mx-auto" data-bs-ride="carousel">
+            <div id="eduportCarousel" className="carousel slide carousel-fade w-75 mx-auto" data-bs-ride="carousel">
               <div className="carousel-inner">
                 <div className="carousel-item active">
+                  <img src="./assets/images/eduport-1.png" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
                   <img src="./assets/images/eduport-2.png" className="d-block w-100" alt="..." />
                 </div>
                 <div className="carousel-item">
@@ -61,23 +70,25 @@ function PortfolioSection() {
                   <img src="./assets/images/eduport-4.png" className="d-block w-100" alt="..." />
                 </div>
               </div>
-              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+              <button className="carousel-control-prev" type="button" data-bs-target="#eduportCarousel" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Previous</span>
               </button>
-              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+              <button className="carousel-control-next" type="button" data-bs-target="#eduportCarousel" data-bs-slide="next">
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Next</span>
               </button>
+
             </div>
           </div>
         )}
       </div>
       <div className="row d-flex justify-content-around align-items-center">
         <div className='col-6'>
-          <div className="portfolio_item col-11" onClick={handlePortfolioItemClick}>
+          <div className="portfolio_item col-11" onClick={handleNetflixItemClick}>
             <div className="portfolio_item_inner">
-              <div className="netflix">
+              <div id="netflix" className="">
+                <img src="./assets/images/netflix-1.png" height={450} width={450} className='rounded-4' alt="" />
                 <ul className="portfolio_categories">
                   <li>
                     <a href="">Figma</a>
@@ -93,30 +104,47 @@ function PortfolioSection() {
                   </li>
                 </ul>
               </div>
-
             </div>
             <h2><a href="https://eduport-black.vercel.app" target="_blank" rel="noopener noreferrer" className='text-light'>Bureau - Architecture Studio Website</a></h2>
           </div>
-          {showCarousel && (
+          {showNetflixCarousel && (
             <div className="carousel">
               <span className="close-button" onClick={handleCloseCarousel}>&times;</span>
-              <div id="carouselExampleFade" className="carousel slide carousel-fade w-75 mx-auto" data-bs-ride="carousel">
+              <div id="netflixCaoursel" className="carousel slide carousel-fade w-75 mx-auto" data-bs-ride="carousel">
                 <div className="carousel-inner">
                   <div className="carousel-item active">
-                    <img src="./assets/images/eduport-2.png" className="d-block w-100" alt="..." />
+                    <img src="./assets/images/netflix-1.png" className="d-block w-100" alt="..." />
                   </div>
                   <div className="carousel-item">
-                    <img src="./assets/images/eduport-3.png" className="d-block w-100" alt="..." />
+                    <img src="./assets/images/netflix-2.png" className="d-block w-100" alt="..." />
                   </div>
                   <div className="carousel-item">
-                    <img src="./assets/images/eduport-4.png" className="d-block w-100" alt="..." />
+                    <img src="./assets/images/netflix-3.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="./assets/images/netflix-4.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="./assets/images/netflix-5.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="./assets/images/netflix-6.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="./assets/images/netflix-7.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="./assets/images/netflix-8.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="./assets/images/netflix-9.png" className="d-block w-100" alt="..." />
                   </div>
                 </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <button className="carousel-control-prev" type="button" data-bs-target="#netflixCaoursel" data-bs-slide="prev">
                   <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                   <span className="visually-hidden">Previous</span>
                 </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <button className="carousel-control-next" type="button" data-bs-target="#netflixCaoursel" data-bs-slide="next">
                   <span className="carousel-control-next-icon" aria-hidden="true"></span>
                   <span className="visually-hidden">Next</span>
                 </button>
@@ -125,39 +153,66 @@ function PortfolioSection() {
           )}
         </div>
         <div className='col-6'>
-          <div className="portfolio_item col-11" onClick={handlePortfolioItemClick}>
-            <div className="portfolio_item_inner netflix">
-              <ul className="portfolio_categories">
-                <li>
-                  <a href="">HTML</a>
-                </li>
-                <li>
-                  <a href="">CSS</a>
-                </li>
-              </ul>
+          <div className="portfolio_item col-11" onClick={handleNetflixItemClick}>
+            <div className="portfolio_item_inner">
+              <div id="netflix" className="">
+                <img src="./assets/images/netflix-1.png" height={450} width={450} className='rounded-4' alt="" />
+                <ul className="portfolio_categories">
+                  <li>
+                    <a href="">Figma</a>
+                  </li>
+                  <li>
+                    <a href="">HTML</a>
+                  </li>
+                  <li>
+                    <a href="">CSS</a>
+                  </li>
+                  <li>
+                    <a href="">Bootstrap</a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <h2><a href="https://netflix-ui-clone-six.vercel.app/index.html" target="_blank" rel="noopener noreferrer" className='text-light'>Bureau - Architecture Studio Website</a></h2>
+            <h2><a href="https://eduport-black.vercel.app" target="_blank" rel="noopener noreferrer" className='text-light'>Bureau - Architecture Studio Website</a></h2>
           </div>
-          {showCarousel && (
+          {showNetflixCarousel && (
             <div className="carousel">
               <span className="close-button" onClick={handleCloseCarousel}>&times;</span>
-              <div id="carouselExampleFade" className="carousel slide carousel-fade w-75 mx-auto" data-bs-ride="carousel">
+              <div id="netflixCaoursel" className="carousel slide carousel-fade w-75 mx-auto" data-bs-ride="carousel">
                 <div className="carousel-inner">
                   <div className="carousel-item active">
-                    <img src="./assets/images/eduport-2.png" className="d-block w-100" alt="..." />
+                    <img src="./assets/images/netflix-1.png" className="d-block w-100" alt="..." />
                   </div>
                   <div className="carousel-item">
-                    <img src="./assets/images/eduport-3.png" className="d-block w-100" alt="..." />
+                    <img src="./assets/images/netflix-2.png" className="d-block w-100" alt="..." />
                   </div>
                   <div className="carousel-item">
-                    <img src="./assets/images/eduport-4.png" className="d-block w-100" alt="..." />
+                    <img src="./assets/images/netflix-3.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="./assets/images/netflix-4.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="./assets/images/netflix-5.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="./assets/images/netflix-6.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="./assets/images/netflix-7.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="./assets/images/netflix-8.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="./assets/images/netflix-9.png" className="d-block w-100" alt="..." />
                   </div>
                 </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <button className="carousel-control-prev" type="button" data-bs-target="#netflixCaoursel" data-bs-slide="prev">
                   <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                   <span className="visually-hidden">Previous</span>
                 </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <button className="carousel-control-next" type="button" data-bs-target="#netflixCaoursel" data-bs-slide="next">
                   <span className="carousel-control-next-icon" aria-hidden="true"></span>
                   <span className="visually-hidden">Next</span>
                 </button>
