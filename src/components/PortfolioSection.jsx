6773ee86222f65386e70986092f8756f2170e8c6
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-03-04 16:46:50 +0500

    add new carousel

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -3,18 +3,30 @@ import React, { useState } from 'react';
 function PortfolioSection() {
   const [showEduportCarousel, setShowEduportCarousel] = useState(false);
   const [showNetflixCarousel, setShowNetflixCarousel] = useState(false);
+  const [showNextjsAuthenticationCarousel, setShowNextjsAuthenticationCarousel] = useState(false);
 
   const handleEduportItemClick = () => {
     setShowEduportCarousel(true);
+    setShowNetflixCarousel(false);
+    setShowNextjsAuthenticationCarousel(false);
   };
 
   const handleNetflixItemClick = () => {
+    setShowEduportCarousel(false);
     setShowNetflixCarousel(true);
+    setShowNextjsAuthenticationCarousel(false);
+  };
+
+  const handleNextjsAuthenticationItemClick = () => {
+    setShowEduportCarousel(false);
+    setShowNetflixCarousel(false);
+    setShowNextjsAuthenticationCarousel(true);
   };
 
   const handleCloseCarousel = () => {
     setShowEduportCarousel(false);
     setShowNetflixCarousel(false);
+    setShowNextjsAuthenticationCarousel(false);
   };
 
   return (
@@ -48,7 +60,6 @@ function PortfolioSection() {
                 </li>
               </ul>
             </div>
-
           </div>
           <h2><a href="https://eduport-black.vercel.app" target="_blank" rel="noopener noreferrer" className='text-light'>Bureau - Architecture Studio Website</a></h2>
         </div>
@@ -78,34 +89,27 @@ function PortfolioSection() {
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Next</span>
               </button>
-
             </div>
           </div>
         )}
       </div>
-      <div className="row d-flex justify-content-around align-items-center">
+      <div className="row d-flex justify-content-around align-items-center me-3">
         <div className='col-6'>
-          <div className="portfolio_item col-11" onClick={handleNetflixItemClick}>
+          <div className="portfolio_item col-10" onClick={handleNetflixItemClick}>
             <div className="portfolio_item_inner">
               <div id="netflix" className="">
-                <img src="./assets/images/netflix-1.png" height={450} width={450} className='rounded-4' alt="" />
+                <img src="./assets/images/netflix-1.png" height={450} width={400} className='rounded-4' alt="" />
                 <ul className="portfolio_categories">
-                  <li>
-                    <a href="">Figma</a>
-                  </li>
                   <li>
                     <a href="">HTML</a>
                   </li>
                   <li>
                     <a href="">CSS</a>
                   </li>
-                  <li>
-                    <a href="">Bootstrap</a>
-                  </li>
                 </ul>
               </div>
             </div>
-            <h2><a href="https://eduport-black.vercel.app" target="_blank" rel="noopener noreferrer" className='text-light'>Bureau - Architecture Studio Website</a></h2>
+            <h2><a href="https://netflix-ui-clone-six.vercel.app/index.html" target="_blank" rel="noopener noreferrer" className='text-light'>Netflix UI Clone</a></h2>
           </div>
           {showNetflixCarousel && (
             <div className="carousel">
@@ -152,11 +156,12 @@ function PortfolioSection() {
             </div>
           )}
         </div>
+
         <div className='col-6'>
-          <div className="portfolio_item col-11" onClick={handleNetflixItemClick}>
+          <div className="portfolio_item col-10" onClick={handleNextjsAuthenticationItemClick}>
             <div className="portfolio_item_inner">
-              <div id="netflix" className="">
-                <img src="./assets/images/netflix-1.png" height={450} width={450} className='rounded-4' alt="" />
+              <div id="authentication" className="">
+                <img src="./assets/images/nextjs-authentication-1.png" height={450} width={400} className='rounded-4' alt="" />
                 <ul className="portfolio_categories">
                   <li>
                     <a href="">Figma</a>
@@ -175,44 +180,26 @@ function PortfolioSection() {
             </div>
             <h2><a href="https://eduport-black.vercel.app" target="_blank" rel="noopener noreferrer" className='text-light'>Bureau - Architecture Studio Website</a></h2>
           </div>
-          {showNetflixCarousel && (
+          {showNextjsAuthenticationCarousel && (
             <div className="carousel">
               <span className="close-button" onClick={handleCloseCarousel}>&times;</span>
-              <div id="netflixCaoursel" className="carousel slide carousel-fade w-75 mx-auto" data-bs-ride="carousel">
+              <div id="authenticationCaoursel" className="carousel slide carousel-fade w-75 mx-auto" data-bs-ride="carousel">
                 <div className="carousel-inner">
                   <div className="carousel-item active">
-                    <img src="./assets/images/netflix-1.png" className="d-block w-100" alt="..." />
+                    <img src="./assets/images/nextjs-authentication-1.png" className="d-block w-100" alt="..." />
                   </div>
                   <div className="carousel-item">
-                    <img src="./assets/images/netflix-2.png" className="d-block w-100" alt="..." />
+                    <img src="./assets/images/nextjs-authentication-2.png" className="d-block w-100" alt="..." />
                   </div>
                   <div className="carousel-item">
-                    <img src="./assets/images/netflix-3.png" className="d-block w-100" alt="..." />
-                  </div>
-                  <div className="carousel-item">
-                    <img src="./assets/images/netflix-4.png" className="d-block w-100" alt="..." />
-                  </div>
-                  <div className="carousel-item">
-                    <img src="./assets/images/netflix-5.png" className="d-block w-100" alt="..." />
-                  </div>
-                  <div className="carousel-item">
-                    <img src="./assets/images/netflix-6.png" className="d-block w-100" alt="..." />
-                  </div>
-                  <div className="carousel-item">
-                    <img src="./assets/images/netflix-7.png" className="d-block w-100" alt="..." />
-                  </div>
-                  <div className="carousel-item">
-                    <img src="./assets/images/netflix-8.png" className="d-block w-100" alt="..." />
-                  </div>
-                  <div className="carousel-item">
-                    <img src="./assets/images/netflix-9.png" className="d-block w-100" alt="..." />
+                    <img src="./assets/images/nextjs-authentication-3.png" className=" d-block w-100" alt="..." />
                   </div>
                 </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#netflixCaoursel" data-bs-slide="prev">
+                <button className="carousel-control-prev" type="button" data-bs-target="#authenticationCaoursel" data-bs-slide="prev">
                   <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                   <span className="visually-hidden">Previous</span>
                 </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#netflixCaoursel" data-bs-slide="next">
+                <button className="carousel-control-next" type="button" data-bs-target="#authenticationCaoursel" data-bs-slide="next">
                   <span className="carousel-control-next-icon" aria-hidden="true"></span>
                   <span className="visually-hidden">Next</span>
                 </button>
@@ -227,4 +214,3 @@ function PortfolioSection() {
 }
 
 export default PortfolioSection;
-
