4a219b0af012d7d474f18e8707a4cf66223e7dae
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-03-05 21:46:11 +0500

    responsive services  section

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -2,39 +2,41 @@ import React from 'react'
 
 function ServicesSection() {
   return (
-    <div className='custom-container my-5 section_space services_section'>
+    <div className='custom-container my-xl-5 my-lg-5 my-md-5 my-3 section_space services_section'>
       <span class="badge rounded-pill section_badge">
         <i class="fa-solid fa-bars-staggered"></i>
         &nbsp; Services
       </span>
-      <div className='w-75'>
+      <div className='title_container'>
         <h2 className='text-light'>
           My <span className='hero_content'>
             Specializations
           </span>
         </h2>
       </div>
-      <div className='services_card text-light'>
-        <span className='d-flex justify-content-between align-items-center'>
-          <p className='title'>
-            Website Design
+      <div className='services_container'>
+        <div className='services_card text-light'>
+          <span className='d-flex justify-content-between align-items-center'>
+            <p className='title'>
+              Website Design
+            </p>
+            <i class="fa-solid fa-bezier-curve fa-xl"></i>
+          </span>
+          <p className='sub_title'>
+            I created digital products with unique ideas use Figma & Framer
           </p>
-          <i class="fa-solid fa-bezier-curve fa-xl"></i>
-        </span>
-        <p className='sub_title'>
-          I created digital products with unique ideas use Figma & Framer
-        </p>
-      </div>
-      <div className='services_card text-light'>
-        <span className='d-flex justify-content-between align-items-center'>
-          <p className='title'>
-            Development
+        </div>
+        <div className='services_card text-light'>
+          <span className='d-flex justify-content-between align-items-center'>
+            <p className='title'>
+              Development
+            </p>
+            <i class="fa-solid fa-code fa-xl"></i>
+          </span>
+          <p className='sub_title'>
+            I build website go live with Framer, Webflow or WordPress
           </p>
-          <i class="fa-solid fa-code fa-xl"></i>
-        </span>
-        <p className='sub_title'>
-          I build website go live with Framer, Webflow or WordPress
-        </p>
+        </div>
       </div>
     </div>
   )
