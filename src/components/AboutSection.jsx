58ac8d6dcd15a7675f11c74334e3db2ee4e9d95b
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-03-05 21:20:41 +0500

    responsive about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function AboutSection() {
   return (
-    <div className='custom-container my-5 section_space'>
+    <div className='custom-container my-5 section_space mx-xl-auto mx-lg-auto mx-md-auto'>
       <span class="badge rounded-pill section_badge">
         <i class="fa-solid fa-user"></i>
         &nbsp; ABOUT
       </span>
-      <div className='w-75'>
+      <div className='title_container'>
         <h2 className='text-light'>
           Every great design begin with
           an even <span className='hero_content'>
@@ -15,7 +15,7 @@ function AboutSection() {
           </span>
         </h2>
       </div>
-      <div style={{ maxWidth: '610px' }}>
+      <div className='subHeading_container'>
         <p className='sub_heading mb-0'>
           Since beginning my journey as a freelance designer nearly 8 years ago, I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chopsone design problem at a time.
         </p>
