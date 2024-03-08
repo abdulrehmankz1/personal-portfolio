ed2abb2b187a0eca524f0dc7e51bf4c9ee35d6be
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-03-08 18:05:13 +0500

    add manubar

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,47 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Link } from 'react-scroll';
 
 function MenuBar() {
   return (
-    <div>
-      <ul class="menu scroll-nav d-flex">
-        <li>
-          <a class="scroll-to" href="#home" data-bs-toggle="tooltip" data-bs-placement="right" title="Home">
-            <i class="fa-solid fa-house"></i>
-          </a>
+    <div className='d-xl-block d-none'>
+      <ul className="menu scroll-nav d-flex">
+        <li className="tip">
+          <Link to="home" smooth={true} duration={500}>
+            <i className="fa-solid fa-house"></i>
+            <span className="tooltiptext">home</span>
+          </Link>
         </li>
-        <li>
-          <a class="scroll-to" href="#about">
-            <i class="fa-solid fa-user"></i>
-          </a>
+        <li className="tip">
+          <Link to="about" smooth={true} duration={500}>
+            <i className="fa-solid fa-user"></i>
+            <span className="tooltiptext">about</span>
+          </Link>
         </li>
-        <li>
-          <a class="scroll-to" href="#resume">
-            <i class="fa-solid fa-briefcase"></i>
-          </a>
+        <li className="tip">
+          <Link to="resume" smooth={true} duration={500}>
+            <i className="fa-solid fa-briefcase"></i>
+            <span className="tooltiptext">resume</span>
+          </Link>
         </li>
-        <li>
-          <a href="#services">
-            <i class="fa-solid fa-bars-staggered"></i>
-          </a>
+        <li className="tip">
+          <Link to="services" smooth={true} duration={500}>
+            <i className="fa-solid fa-envelope"></i>
+            <span className="tooltiptext">services</span>
+          </Link>
         </li>
-        <li>
-          <a class="scroll-to" href="#skills">
-            <i class="fa-solid fa-shapes"></i>
-          </a>
+        <li className="tip">
+          <Link to="skills" smooth={true} duration={500}>
+            <i className="fa-solid fa-bars-staggered"></i>
+            <span className="tooltiptext">skills</span>
+          </Link>
         </li>
-        <li>
-          <a class="scroll-to active" href="#portfolio">
+        <li className="tip">
+          <Link to="portfolio" smooth={true} duration={500}>
+            <i className="fa-solid fa-shapes"></i>
+            <span className="tooltiptext">portfolio</span>
+          </Link>
+        </li>
+        <li className="tip">
+          <Link to="contact" smooth={true} duration={500}>
             <i className="fa-solid fa-grip-vertical"></i>
-          </a>
-        </li>
-        <li>
-          <a class="scroll-to" href="#contact" >
-            <i class="fa-solid fa-envelope"></i>
-          </a>
+            <span className="tooltiptext">Contact</span>
+          </Link>
         </li>
       </ul>
+
     </div>
-  )
+  );
 }
 
-export default MenuBar
+export default MenuBar;
